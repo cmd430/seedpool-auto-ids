cd0aa43d89504f0fbf68ac8aec2d814cef65f8b6
cmd430 <cmd430@gmail.com>
2026-04-07 05:58:30 +1200

    refactor to use more helpers

--- src/UNIT3D.ts
+++ src/UNIT3D.ts
@@ -32,10 +32,8 @@ export class UNIT3D {
       this.createMetadata(idSource, id)
     } else if (location.pathname.endsWith('edit')) {
       this.editMetadata(idSource, id)
-    } else if (location.pathname.includes('similar')) {
+    } else if (location.pathname.includes('similar') || location.pathname.endsWith('torrents')) {
       this.bulkEditMetadata(idSource, id)
-    } else if (location.pathname.endsWith('torrents')) {
-      this.bulkEditMetadata(idSource, id, true)
     }
   }
 
@@ -44,111 +42,76 @@ export class UNIT3D {
   }
 
   private async editMetadata (idSource: IdSource, id: string) {
-    const mediaType = this.getMediaTypeFromCategory(document.querySelector<HTMLSelectElement>('#category_id')?.value)
-    if (!mediaType) return
-
-    const ids = await this.api.getIds(id, idSource, mediaType)
-    if (!ids) return
-
-    // auto fill tmdb id
-    const exists_on_tmdb = document.querySelector<HTMLInputElement>(`#${mediaType}_exists_on_tmdb`)
-    const tmdb_input = document.querySelector<HTMLInputElement>(`#tmdb_${mediaType}_id`)
-    if (ids.id) {
-      this.setCheckboxChecked(exists_on_tmdb!, true)
-      await wait({ milliseconds: 100 })
-      this.setInputValue(tmdb_input!, String(ids.id))
-    } else {
-      this.setInputValue(tmdb_input!, '')
-      await wait({ milliseconds: 100 })
-      this.setCheckboxChecked(exists_on_tmdb!, false)
-    }
-
-    // auto fill imdb id
-    const exists_on_imdb = document.querySelector<HTMLInputElement>('#title_exists_on_imdb')
-    const imdb_input = document.querySelector<HTMLInputElement>('#imdb')
-    if (ids.imdb_id) {
-      this.setCheckboxChecked(exists_on_imdb!, true)
-      await wait({ milliseconds: 100 })
-      this.setInputValue(imdb_input!, ids.imdb_id.slice(2))
-    } else {
-      this.setInputValue(imdb_input!, '')
-      await wait({ milliseconds: 100 })
-      this.setCheckboxChecked(exists_on_imdb!, false)
-    }
-
-    // auto fill tvdb id
-    const exists_on_tvdb = document.querySelector<HTMLInputElement>('#tv_exists_on_tvdb')
-    const tvdb_input = document.querySelector<HTMLInputElement>('#tvdb')
-    if (mediaType === 'tv' && ids.tvdb_id) {
-      this.setCheckboxChecked(exists_on_tvdb!, true)
-      await wait({ milliseconds: 100 })
-      this.setInputValue(tvdb_input!, String(ids.tvdb_id))
-    } else if (mediaType === 'tv' && !ids.tvdb_id) {
-      this.setInputValue(tvdb_input!, '')
-      await wait({ milliseconds: 100 })
-      this.setCheckboxChecked(exists_on_tvdb!, false)
-    }
-
-    // auto fill mal id
-    const exists_on_mal = document.querySelector<HTMLInputElement>('#anime_exists_on_mal')
-    const mal_input = document.querySelector<HTMLInputElement>('#mal')
-    if (ids.mal_id) {
-      this.setCheckboxChecked(exists_on_mal!, true)
-      await wait({ milliseconds: 100 })
-      this.setInputValue(mal_input!, String(ids.mal_id))
-    } else {
-      this.setInputValue(mal_input!, '')
-      await wait({ milliseconds: 100 })
-      this.setCheckboxChecked(exists_on_mal!, false)
-    }
-  }
-
-  private async bulkEditMetadata (idSource: IdSource, id: string, isSearch = false) {
-    let mediaType: MediaType | undefined
-    if (isSearch) {
-      mediaType = this.getMediaTypeFromCategory(document.querySelector<HTMLElement>('.data-table tr[data-torrent-id]:has(input:checked)')?.dataset.categoryId)
-    } else {
-      mediaType = this.getMediaTypeFromText(document.querySelector<HTMLLabelElement>('#swal2-html-container > div > div:first-of-type > label')?.textContent)
-    }
+    const mediaType = this.getMediaType()
     if (mediaType !== 'tv' && mediaType !== 'movie') return
 
     const ids = await this.api.getIds(id, idSource, mediaType)
     if (!ids) return
 
-    // auto fill tmdb id
-    const tmdb_input = document.querySelector<HTMLInputElement>('#bulk-tmdb-id')
-    if (ids.id) {
-      this.setInputValue(tmdb_input!, String(ids.id))
-    } else {
-      this.setInputValue(tmdb_input!, '')
-    }
+    await this.editId(`#${mediaType}_exists_on_tmdb`, `#tmdb_${mediaType}_id`, ids.id)
+    await this.editId('#title_exists_on_imdb', '#imdb', ids.imdb_id?.slice(2))
+    await this.editId('#tv_exists_on_tvdb', '#tvdb', ids.tvdb_id)
+    await this.editId('#anime_exists_on_mal', '#mal', ids.mal_id)
+  }
 
-    // auto fill imdb id
-    const imdb_input = document.querySelector<HTMLInputElement>('#bulk-imdb-id')
-    if (ids.imdb_id) {
-      this.setInputValue(imdb_input!, ids.imdb_id.slice(2))
-    } else {
-      this.setInputValue(imdb_input!, '')
-    }
+  private async editId (existsSelector: string, inputSelector: string, id: string | number | undefined | null) {
+    const exists = document.querySelector<HTMLInputElement>(existsSelector)
+    const input = document.querySelector<HTMLInputElement>(inputSelector)
 
-    // auto fill tvdb id
-    const tvdb_input = document.querySelector<HTMLInputElement>('#bulk-tvdb-id')
-    if (mediaType === 'tv' && ids.tvdb_id) {
-      this.setInputValue(tvdb_input!, String(ids.tvdb_id))
-    } else if (mediaType === 'tv' && !ids.tvdb_id) {
-      this.setInputValue(tvdb_input!, '')
-    }
-
-    // auto fill mal id
-    const mal_input = document.querySelector<HTMLInputElement>('#bulk-mal-id')
-    if (ids.mal_id) {
-      this.setInputValue(mal_input!, String(ids.mal_id))
+    if (id) {
+      this.setCheckboxChecked(exists, true)
+      await wait({ milliseconds: 100 })
+      this.setInputValue(input, String(id))
     } else {
-      this.setInputValue(mal_input!, '')
+      this.setInputValue(input!, '')
+      await wait({ milliseconds: 100 })
+      this.setCheckboxChecked(exists, false)
     }
   }
 
-  private getMediaTypeFromCategory (category?: string) {
+  private async bulkEditMetadata (idSource: IdSource, id: string) {
+    const mediaType = this.getMediaType()
+    if (mediaType !== 'tv' && mediaType !== 'movie') return
+
+    const ids = await this.api.getIds(id, idSource, mediaType)
+    if (!ids) return
+
+    await this.bulkEditId('#bulk-tmdb-id', ids.id)
+    await this.bulkEditId('#bulk-imdb-id', ids.imdb_id?.slice(2))
+    await this.bulkEditId('#bulk-tvdb-id', ids.tvdb_id)
+    await this.bulkEditId('#bulk-mal-id', ids.mal_id)
+  }
+
+  private async bulkEditId (inputSelector: string, id: string | number | undefined | null) {
+    const input = document.querySelector<HTMLInputElement>(inputSelector)
+
+    if (id) {
+      this.setInputValue(input, String(id))
+    } else {
+      this.setInputValue(input, '')
+    }
+  }
+
+  private getMediaType () {
+    let mediaType = this.getMediaTypeFromCategoryId(document.querySelector<HTMLSelectElement>('#category_id')?.value)
+
+    if (!mediaType) {
+      mediaType = this.getMediaTypeFromCategoryId(document.querySelector<HTMLElement>('.data-table tr[data-torrent-id]:has(input:checked)')?.dataset.categoryId)
+    }
+
+    if (!mediaType) {
+      mediaType = this.getMediaTypeFromText(document.querySelector<HTMLLabelElement>('#swal2-html-container > div > div:first-of-type > label')?.textContent)
+    }
+
+    if (!mediaType) {
+      mediaType = this.getMediaTypeFromCategoryId(document.querySelector<HTMLAnchorElement>('.work__media-type-link')?.href.slice(-1))
+
+    }
+
+    return mediaType
+  }
+
+  private getMediaTypeFromCategoryId (category?: string) {
     if (!category) return
     return <MediaType>{ 1: 'movie', 2: 'tv', 6: 'tv' }[category]
   }
@@ -158,13 +121,13 @@ export class UNIT3D {
     return <MediaType>text.match(/(Movie|TV)/i)?.[1].toLowerCase()
   }
 
-  private setInputValue (input: HTMLInputElement, value: string) {
+  private setInputValue (input: HTMLInputElement | null, value: string) {
     if (!input) return
     input.value = value ?? ''
     input.dispatchEvent(new Event('change'))
   }
 
-  private setCheckboxChecked (checkbox: HTMLInputElement, checked: boolean) {
+  private setCheckboxChecked (checkbox: HTMLInputElement | null, checked: boolean) {
     if (!checkbox) return
     checkbox.checked = checked
     checkbox.dispatchEvent(new Event('change'))
