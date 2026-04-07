f826ab0d66542b719121058ba35bad7ccfe0ef27
cmd430 <cmd430@gmail.com>
2026-04-07 18:01:44 +1200

    more advanced page matching

--- src/seedpoolAutoIds.ts
+++ src/seedpoolAutoIds.ts
@@ -2,8 +2,22 @@ import { TMDB_API_KEY } from './secrets'
 import { API } from './API'
 import { UNIT3D } from './UNIT3D'
 
-new UNIT3D({
-  api: new API({
-    tmdbKey: TMDB_API_KEY
+const validPages = [
+  /\/torrents$/,
+  /\/torrents\/\d+$/,
+  /\/similar\/[\d.]+$/,
+  /\/edit$/,
+  /\/create$/
+]
+
+let unit3d: UNIT3D | undefined
+for (const page of validPages) {
+  if (!page.test(location.pathname) || unit3d) continue
+
+  // ony run when page is in the list of valid pages
+  unit3d = new UNIT3D({
+    api: new API({
+      tmdbKey: TMDB_API_KEY
+    })
   })
-})
+}
